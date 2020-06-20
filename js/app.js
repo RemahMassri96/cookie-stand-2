f8f2a033949f4f0d957be932c6e727764bf1ccbd
remah massri <remah.massri@gmail.com>
2020-06-20 11:40:47 +0300

    add table

--- js/app.js
+++ js/app.js
@@ -11,16 +11,61 @@ function CookieStand(locationName,minNumOfCustomers,maxNumOfCustomer,avgNumOfCoo
  this.avgNumOfCookiesPerCustomer =avgNumOfCookiesPerCustomer;
  this.customersPerHour =[];
  this.cookiesPerHour =[];
+ this.totalCookiesPerDay = 0;
  allLocations.push(this);
 
 }
 
-//CookieStand.prototype.generateNumOfCustomers() {
+// a function that fills the customer per hour array
+CookieStand.prototype.generateNumOfCustomers= function() {
+   for(var i =0; i< workingHours.length;i++){
+     this.customersPerHour.push(generateRandomNumber(this.minNumOfCustomers,this.maxNumOfCustomer));
 
-//}
+   }
+};
+
+CookieStand.prototype.calculateSoldCookies = function(){
+  for(var i =0; i< workingHours.length; i++){
+    var numberfCookies = Math.ceil(this.avgNumOfCookiesPerCustomer * this.customersPerHour[i]);
+    this.cookiesPerHour.push(numberfCookies);
+    this.totalCookiesPerDay = this.totalCookiesPerDay + numberfCookies;
+
+
+  }
+};
+
+
+ function makeHeaderRow(){
+   var headerRowElement = document.createElement('tr');
+   var th = document.createElement('th');
+   th.textContent ='';
+   headerRowElement.appendChild(th);
+
+   for(var i=0; i<workingHours.length; i++){
+  
+     th = document.createElement('th');
+     th.textContent = workingHours[i];
+     headerRowElement.appendChild(th);
+   }
+
+   th = document.createElement('th');
+
+
+ }
+
+// create an object 
+
+for(var i =0; i<allLocations.length;i++){
+  allLocations[i].generateNumOfCustomers();
+  allLocations[i].calculateSoldCookies();
+  console.log(allLocations[i]);
+  
+
+}
+
 
 // a function that return a random value between minimum and maximum
 function generateRandomNumber(min, max){
-   return (Math.floor(Math.random) * (max- + 1) + min);
+   return Math.floor(Math.random() * (max- + 1) + min);
 }
 
